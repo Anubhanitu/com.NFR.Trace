12c1589480cb01a3c9ec552547a52f770af94023
Anubhanitu <anubha@paramountsoft.net>
2024-05-16 20:16:31 +0530

    adding files

--- src/main/java/com/demo/baseutil/Initializepages.java
+++ src/main/java/com/demo/baseutil/Initializepages.java
@@ -2,6 +2,14 @@ package com.demo.baseutil;
 
 import org.openqa.selenium.WebDriver;
 
+import com.demo.pages.Currentstock;
+import com.demo.pages.Importsales;
+import com.demo.pages.Inventorycapacity;
+import com.demo.pages.Inventoryquotation;
+import com.demo.pages.Salesdata;
+import com.demo.pages.Stockedinlot;
+import com.demo.pages.Stockin;
+import com.demo.pages.Stockout;
 import com.demo.pages.Tracelogin;
 import com.demo.pages.Tracelogin2;
 import com.demo.pages.Tracesigninpage;
@@ -24,6 +32,14 @@ public class Initializepages extends Basetest2 {
 	public Tracelogout logout;
 	public Tracelogin2 loginpage2;
 	public Tracesignuppage signup;
+	public Inventoryquotation quotationpage;
+	public Inventorycapacity inventorypage;
+	public Stockin stockinpage;
+	public Stockout stockoutpage;
+	public Currentstock currentpage;
+	public Salesdata salespage;
+	public Importsales importsalespage;
+	public Stockedinlot stockdetailpage;
 	
 	
 	public Initializepages(WebDriver driver, long eto, WebActionUtil actionutil) {
@@ -33,6 +49,14 @@ public class Initializepages extends Basetest2 {
 		loginpage2 = new Tracelogin2(driver, eto, actionutil);
 		logout = new Tracelogout(driver, eto, actionutil);
 		signup= new Tracesignuppage(driver, eto,actionutil);
+		inventorypage = new Inventorycapacity(driver, eto,actionutil);
+		quotationpage = new Inventoryquotation(driver, eto,actionutil);
+		stockinpage = new Stockin(driver, eto,actionutil);
+		stockoutpage = new Stockout(driver, eto,actionutil);
+		currentpage = new Currentstock(driver, eto,actionutil);
+		salespage = new Salesdata(driver, eto,actionutil);
+		importsalespage = new Importsales(driver, eto,actionutil);
+		stockdetailpage = new Stockedinlot(driver, eto,actionutil);
 		
 	}
 
